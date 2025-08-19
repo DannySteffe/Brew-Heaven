e85a4263ead6c8ca4dd11e8dc60b3eda82b4fdd6
Danny Steffe S <dannysteffe1465@gmail.com>
2025-08-18 20:38:44 -0800

    tailwind

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <button className='bg-blue-500'>hello</button>
+    </>
+  )
+}
+
+export default App
